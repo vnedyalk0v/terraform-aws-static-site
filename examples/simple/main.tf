812d0fd5789443f2b4fe18629005e0caede72eb7
Geori Nedyalkov <vnedyalk0v@proton.me>
2025-03-08 16:59:24 +0200

    Remove random_string.suffix

--- examples/simple/main.tf
+++ examples/simple/main.tf
@@ -2,17 +2,10 @@ provider "aws" {
   region = "eu-central-1"
 }
 
-# Generate a random suffix for the S3 bucket name
-resource "random_string" "suffix" {
-  length  = 8
-  special = false
-  upper   = false
-}
-
 # Minimal configuration - just specify the bucket name
 module "static_website" {
   source = "../../"
 
   # Only specify the bucket name
-  bucket_name = "my-static-website-bucket-${random_string.suffix.result}"
+  bucket_name = "my-static-website-bucket-amjqrwcf"
 }
